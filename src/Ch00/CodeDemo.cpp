6e755c5a3f691c9743a5fb6fcb1e3d270fd3237f
jjovanw <jjovanw@gmail.com>
2025-03-18 03:42:32 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 modified with comment
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
